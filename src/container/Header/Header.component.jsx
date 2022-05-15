3aa3c9e57723d6cac9c5ce2b01bc1821669c0dfe
Jeremy Agcaoili <agcaoili.jeremy@gmail.com>
2022-05-15 14:35:58 +0800

    Update home component

--- src/container/Header/Header.component.jsx
+++ src/container/Header/Header.component.jsx
@@ -16,8 +16,34 @@ function Header() {
     },
   };
   return (
-    <div className="app__header app__flex" id="home">
-      <motion.div
+    <div className="app__header" id="home">
+      <div className="app__header-introduction">
+        <motion.div
+          whileInView={{ y: [100, 0], opacity: [0, 1] }}
+          transition={{ duration: 1 }}
+          viewport={{ once: true }}
+        >
+          <p className="p-text">Mabuhay!</p>
+        </motion.div>
+        <motion.div
+          whileInView={{ y: [100, 0], opacity: [0, 1] }}
+          transition={{ duration: 1, delay: 1 }}
+          viewport={{ once: true }}
+        >
+          <h1 className="head-text">
+            I am Jeremy <span>Agcaoili</span>
+          </h1>
+        </motion.div>
+        <motion.div
+          whileInView={{ y: [100, 0], opacity: [0, 1] }}
+          transition={{ duration: 1, delay: 2 }}
+          viewport={{ once: true }}
+        >
+          <p className="p-text">I'm a passionate Software Engineer!</p>
+        </motion.div>
+      </div>
+
+      {/* <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.5 }}
         className="app__header-info"
@@ -60,7 +86,7 @@ function Header() {
             <img src={circle} alt="technologies logo" />
           </div>
         ))}
-      </motion.div>
+      </motion.div> */}
     </div>
   );
 }
